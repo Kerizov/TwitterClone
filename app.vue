224e81fb92be8e62199ad7c2aa3a625b135b13f9
Kerizov <stepanok980@gmail.com>
2022-09-19 14:58:11 +0300

    Added image of logo

--- app.vue
+++ app.vue
@@ -1,7 +1,14 @@
 <template>
   <div :class="{'dark': darkMode}">
     <div class="bg-white dark:bg-dim-900">
-
+      <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+        <!--     Left sidebar   -->
+        <div class="hidden md:block xs-col-span-1 xl:col-span-2">
+          <div class="sticky top-0">
+            <SidebarLeft/>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
