ce18bf13e72aee4c6346cf147b39e3a05cadccd3
Kerizov <stepanok980@gmail.com>
2022-10-21 08:39:09 +0300

    Added the paths to all of template files in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [],
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./app.vue"
+  ],
   darkMode: 'class',
 
   theme: {
